9068ced9a7dc279bd124d20f572ec7ffec22ae65
Parasuram, Prakash (Prakash) <prakash@nationwide.com>
2019-08-24 14:21:49 -0400

    prakash Java1Lab2Git commit 1

---
+++ Test/edu/cscc/TestJava1Lab2Git.java
@@ -0,0 +1,13 @@
+package edu.cscc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestJava1Lab2Git {
+    @Test
+    public void givenNothingTestJava1Lab2Git(){
+        HelloWorld helloW = new HelloWorld();
+        assertEquals("", HelloWorld)
+    }
+}
